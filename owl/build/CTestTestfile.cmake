0b2b6c8183bec0782ab5a41789873963a97b523d
Tamizhanban212 <tamizhanbanag@gmail.com>
2024-09-14 15:39:58 +0530

    added owl workspace

---
+++ owl/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tamizhanban/Documents/ITR/owl/src
+# Build directory: /home/tamizhanban/Documents/ITR/owl/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ITR-OWL/owl_robot_description")
---
+++ owl/build/ITR-OWL/owl_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tamizhanban/Documents/ITR/owl/src/ITR-OWL/owl_robot_description
+# Build directory: /home/tamizhanban/Documents/ITR/owl/build/ITR-OWL/owl_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
