f26bed1b90e510f759fcac99cf90b6072a72f31c
Aarav2708 <aarav.shah@iitgn.ac.in>
2024-10-19 10:32:22 -0700

    Added RL Ws

---
+++ addverb_rl/build/Object_Impedance_Control_Dual_Arm_Addverb/heal_right/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lheal_right".split(';') if "-lheal_right" != "" else []
+PROJECT_NAME = "heal_right"
+PROJECT_SPACE_DIR = "/home/aarav/ITR/addverb_rl/devel"
+PROJECT_VERSION = "0.0.0"
