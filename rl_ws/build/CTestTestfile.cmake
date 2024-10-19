6579896bec96875e9d0779d3a5c8be5811d90541
Tamizhanban212 <tamizhanbanag@gmail.com>
2024-10-19 12:22:39 +0530

    added rl_ws

---
+++ rl_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tamizhanban/Documents/ITR/rl_ws/src
+# Build directory: /home/tamizhanban/Documents/ITR/rl_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
