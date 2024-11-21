363d1eeb0e5b6f72ce092d74f26a63366d4e8258
Tamizhanban212 <tamizhanbanag@gmail.com>
2024-11-21 23:16:16 +0530

    pushed trans_mat

--- itr-leaphand-graspobject/IK_bro/trans_matrix.py
+++ itr-leaphand-graspobject/IK_bro/trans_matrix.py
@@ -1,4 +1,23 @@
 import numpy as np
 
 class TransMatrix:
-    def 
+    @staticmethod
+    def compute_obj_pos(object_pose_cam, palm_wrt_cam):
+        """
+        Computes the position of the object with respect to the palm.
+
+        Parameters:
+        - object_pose_cam: numpy array, the 4x4 transformation matrix of the object's pose in the camera frame.
+        - palm_wrt_cam: numpy array, the 4x4 transformation matrix of the palm's pose in the camera frame.
+
+        Returns:
+        - numpy array: The position (x, y, z) of the object with respect to the palm frame.
+        """
+        # Compute the transformation matrix of the object with respect to the palm
+        obj_wrt_palm = np.linalg.inv(palm_wrt_cam) @ object_pose_cam
+        
+        # Extract the (x, y, z) position from the last column of the transformation matrix
+        x, y, z = obj_wrt_palm[0, 3], obj_wrt_palm[1, 3], obj_wrt_palm[2, 3]
+        
+        return np.array([x, y, z])
+    
