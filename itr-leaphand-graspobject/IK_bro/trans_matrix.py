7e76ee3322823a56e0d465772c4628e56df377f2
Aarav2708 <aarav.shah@iitgn.ac.in>
2024-11-22 04:57:47 +0530

    compute contact points

--- itr-leaphand-graspobject/IK_bro/trans_matrix.py
+++ itr-leaphand-graspobject/IK_bro/trans_matrix.py
@@ -20,4 +20,32 @@ class TransMatrix:
         x, y, z = obj_wrt_palm[0, 3], obj_wrt_palm[1, 3], obj_wrt_palm[2, 3]
         
         return np.array([x, y, z])
-    
+
+    @staticmethod
+    def compute_contact_points(object_pose_cam, palm_wrt_cam, offsets):
+        """
+        Computes the contact points of the palm with the object.
+
+        Parameters:
+        - object_pose_cam: numpy array, the 4x4 transformation matrix of the object's pose in the camera frame.
+        - palm_wrt_cam: numpy array, the 4x4 transformation matrix of the palm's pose in the camera frame.
+        - offsets: list of numpy arrays, offsets [b1, b2] to compute the contact points.
+
+        Returns:
+        - list of numpy arrays: The contact points of the palm with the object.
+        """
+        # Compute the transformation matrix of the object with respect to the palm
+        obj_wrt_palm = np.linalg.inv(palm_wrt_cam) @ object_pose_cam
+
+        # Extract the object's position and orientation with respect to the palm
+        obj_position = obj_wrt_palm[:3, 3]  # Position (x, y, z)
+        obj_orientation = obj_wrt_palm[:3, :3]  # Orientation matrix (3x3)
+
+        # Compute contact points by applying offsets to the object's position
+        contact_points = []
+        for offset in offsets:
+            # Transform the offset into the palm frame
+            contact_point = obj_position + obj_orientation @ offset.flatten()
+            contact_points.append(contact_point)
+
+        return contact_points
